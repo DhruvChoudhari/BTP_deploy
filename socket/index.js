7a38e10dd196a8800ca06c876fe35d30fd4b89bb
Dhruv Choudhari <103051702+DhruvChoudhari@users.noreply.github.com>
2025-03-31 15:47:52 +0530

    trying to push to main

--- socket/index.js
+++ socket/index.js
@@ -108,7 +108,6 @@ io.on("connection", (socket) => {
     io.emit("getUsers", users);
   });
 });
-
 server.listen(process.env.PORT || 4000, () => {
   console.log(`server is running on port ${process.env.PORT || 4000}`);
 });
