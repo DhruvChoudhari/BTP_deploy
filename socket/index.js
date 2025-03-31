10a240179d5d8791d35a11a6d44648eeb6287f86
Dhruv Choudhari <103051702+DhruvChoudhari@users.noreply.github.com>
2025-03-31 15:43:01 +0530

    push whole

--- socket/index.js
+++ socket/index.js
@@ -104,6 +104,7 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log(`a user disconnected!`);
     removeUser(socket.id);
+    
     io.emit("getUsers", users);
   });
 });
